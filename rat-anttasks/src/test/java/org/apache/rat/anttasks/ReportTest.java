e8a0555aa2bf6f3b30172f5a3f437b260dcdfb77
Stefan Bodewig <bodewig@apache.org>
2009-02-10 09:53:30 +0000

    Make JUnit tests pass on Windows
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rat/main/trunk/rat@742920 13f79535-47bb-0310-9956-ffa450edef68

--- rat-anttasks/src/test/java/org/apache/rat/anttasks/ReportTest.java
+++ rat-anttasks/src/test/java/org/apache/rat/anttasks/ReportTest.java
@@ -8,61 +8,65 @@ import org.apache.tools.ant.BuildException;
 import junit.framework.Assert;
 
 public class ReportTest extends AbstractRatAntTaskTest {
-	private static final File antFile = new File("src/test/resources/antunit/report-junit.xml").getAbsoluteFile();
+    private static final File antFile = new File("src/test/resources/antunit/report-junit.xml").getAbsoluteFile();
 
-	protected File getAntFile() {
-		return antFile;
-	}
+    protected File getAntFile() {
+        return antFile;
+    }
 
-	public void testWithReportSentToAnt() throws Exception {
-		executeTarget("testWithReportSentToAnt");
-		assertLogMatches("AL +\\Q" + getAntFile().getPath() + "\\E");
-	}
+    public void testWithReportSentToAnt() throws Exception {
+        executeTarget("testWithReportSentToAnt");
+        assertLogMatches("AL +\\Q" + getAntFileName() + "\\E");
+    }
 
-	public void testWithReportSentToFile() throws Exception {
-		final File reportFile = new File(getTempDir(), "selftest.report");
-		getTempDir().mkdirs();
-		final String alLine = "AL +\\Q" + getAntFile().getPath() + "\\E";
-		if (reportFile.isFile()  &&  !reportFile.delete()) {
-			throw new IOException("Unable to remove report file " + reportFile);
-		}
-		executeTarget("testWithReportSentToFile");
-		assertLogDoesntMatch(alLine);
-		Assert.assertTrue("Expected report file " + reportFile, reportFile.isFile());
-		assertFileMatches(reportFile, alLine);
-	}
+    public void testWithReportSentToFile() throws Exception {
+        final File reportFile = new File(getTempDir(), "selftest.report");
+        getTempDir().mkdirs();
+        final String alLine = "AL +\\Q" + getAntFileName() + "\\E";
+        if (reportFile.isFile()  &&  !reportFile.delete()) {
+            throw new IOException("Unable to remove report file " + reportFile);
+        }
+        executeTarget("testWithReportSentToFile");
+        assertLogDoesntMatch(alLine);
+        Assert.assertTrue("Expected report file " + reportFile, reportFile.isFile());
+        assertFileMatches(reportFile, alLine);
+    }
 
-	public void testWithASLUnknown() throws Exception {
-		executeTarget("testWithASLUnknown");
-		assertLogDoesntMatch("AL +\\Q" + getAntFile().getPath() + "\\E");
-		assertLogMatches("\\!\\?\\?\\?\\?\\? +\\Q" + getAntFile().getPath() + "\\E");
-	}
+    public void testWithASLUnknown() throws Exception {
+        executeTarget("testWithASLUnknown");
+        assertLogDoesntMatch("AL +\\Q" + getAntFileName() + "\\E");
+        assertLogMatches("\\!\\?\\?\\?\\?\\? +\\Q" + getAntFileName() + "\\E");
+    }
 
-	public void testCustomMatcher() throws Exception {
-		executeTarget("testCustomMatcher");
-		assertLogDoesntMatch("AL +\\Q" + getAntFile().getPath() + "\\E");
-		assertLogMatches("EXMPL +\\Q" + getAntFile().getPath() + "\\E");
-	}
+    public void testCustomMatcher() throws Exception {
+        executeTarget("testCustomMatcher");
+        assertLogDoesntMatch("AL +\\Q" + getAntFileName() + "\\E");
+        assertLogMatches("EXMPL +\\Q" + getAntFileName() + "\\E");
+    }
 
-	public void testNoResources() throws Exception {
-		try {
-			executeTarget("testNoResources");
-			fail("Expected Exception");
-		} catch (BuildException e) {
-			final String expect = "You must specify at least one file";
-			assertTrue("Expected " + expect + ", got " + e.getMessage(),
-					e.getMessage().indexOf(expect) != -1);
-		}
-	}
+    public void testNoResources() throws Exception {
+        try {
+            executeTarget("testNoResources");
+            fail("Expected Exception");
+        } catch (BuildException e) {
+            final String expect = "You must specify at least one file";
+            assertTrue("Expected " + expect + ", got " + e.getMessage(),
+                       e.getMessage().indexOf(expect) != -1);
+        }
+    }
 
-	public void testNoLicenseMatchers() throws Exception {
-		try {
-			executeTarget("testNoLicenseMatchers");
-			fail("Expected Exception");
-		} catch (BuildException e) {
-			final String expect = "at least one license";
-			assertTrue("Expected " + expect + ", got " + e.getMessage(),
-					e.getMessage().indexOf(expect) != -1);
-		}
-	}
+    public void testNoLicenseMatchers() throws Exception {
+        try {
+            executeTarget("testNoLicenseMatchers");
+            fail("Expected Exception");
+        } catch (BuildException e) {
+            final String expect = "at least one license";
+            assertTrue("Expected " + expect + ", got " + e.getMessage(),
+                       e.getMessage().indexOf(expect) != -1);
+        }
+    }
+
+    private String getAntFileName() {
+        return getAntFile().getPath().replace('\\', '/');
+    }
 }
