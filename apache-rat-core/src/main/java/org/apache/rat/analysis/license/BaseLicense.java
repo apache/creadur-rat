842ce4e683a4470f592848cbcb3d3cc60a7274c7
Sebastian Bazley <sebb@apache.org>
2013-05-19 13:43:45 +0000

    Unused
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/trunk@1484285 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/analysis/license/BaseLicense.java
+++ apache-rat-core/src/main/java/org/apache/rat/analysis/license/BaseLicense.java
@@ -18,7 +18,6 @@
  */ 
 package org.apache.rat.analysis.license;
 
-import org.apache.rat.analysis.RatHeaderAnalysisException;
 import org.apache.rat.api.Document;
 import org.apache.rat.api.MetaData;
 
