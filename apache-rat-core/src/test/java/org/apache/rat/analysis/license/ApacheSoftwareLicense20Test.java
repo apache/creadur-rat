c031688dcd033a3a9eb3ecef6b71f8157b1a5805
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-06 09:40:54 +0000

    Add test: testApacheLicenseIllegalStateFamilyCategory,testApacheLicenseIllegalStateFamilyName
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1548468 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/analysis/license/ApacheSoftwareLicense20Test.java
+++ apache-rat-core/src/test/java/org/apache/rat/analysis/license/ApacheSoftwareLicense20Test.java
@@ -18,7 +18,10 @@
  */ 
 package org.apache.rat.analysis.license;
 
+import org.apache.rat.analysis.license.BaseLicense;
 import org.apache.rat.api.Document;
+import org.apache.rat.api.MetaData;
+import org.apache.rat.api.MetaData.Datum;
 import org.apache.rat.document.MockLocation;
 import org.junit.Before;
 import org.junit.Test;
@@ -63,6 +66,25 @@ public class ApacheSoftwareLicense20Test {
 			assertFalse("Not Match the reference apache license", worker.match(
 					subject, "'Behold, Telemachus! (nor fear the sight,)"));
 		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testApacheLicenseIllegalStateFamilyCategory() {
+		Datum licenseFamilyCategory = new Datum("", "");
+		Datum licenseFamilyName = new Datum("", "");
+		String notes = null;
+		new BaseLicense(licenseFamilyCategory,
+				licenseFamilyName, notes);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testApacheLicenseIllegalStateFamilyName() {
+		Datum licenseFamilyCategory = new Datum(
+				MetaData.RAT_URL_LICENSE_FAMILY_CATEGORY,
+				MetaData.RAT_URL_LICENSE_FAMILY_CATEGORY);
+		Datum licenseFamilyName = new Datum("", "");
+		String notes = null;
+		new BaseLicense(licenseFamilyCategory, licenseFamilyName, notes);
 	}
 
 }
