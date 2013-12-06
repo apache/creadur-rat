b6bc3d8ca4fc191032d9fa72b3f59f826cc743c7
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-06 08:22:04 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1548406 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/document/CompositeDocumentException.java
+++ apache-rat-core/src/main/java/org/apache/rat/document/CompositeDocumentException.java
@@ -15,25 +15,24 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.document;
 
 import java.io.IOException;
 
 /**
- * Indicates that the document is a composite archive 
- * and cannot be read.
+ * Indicates that the document is a composite archive and cannot be read.
  */
 public class CompositeDocumentException extends IOException {
 
-    private static final long serialVersionUID = -8874256971728263295L;
+	private static final long serialVersionUID = -8874256971728263295L;
 
-    public CompositeDocumentException() {
-        super("This document must be read as an archive.");
-    }
+	public CompositeDocumentException() {
+		super("This document must be read as an archive.");
+	}
 
-    public CompositeDocumentException(String s) {
-        super(s);
-    }
+	public CompositeDocumentException(String s) {
+		super(s);
+	}
 
 }
