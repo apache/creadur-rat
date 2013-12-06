891daede4458d146615c9096a6de4b621b678601
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-06 09:43:19 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1548469 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/analysis/license/AppliedApacheSoftwareLicense20Test.java
+++ apache-rat-core/src/test/java/org/apache/rat/analysis/license/AppliedApacheSoftwareLicense20Test.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.analysis.license;
 
 import java.io.BufferedReader;
@@ -33,70 +33,72 @@ import static org.junit.Assert.assertTrue;
 
 public class AppliedApacheSoftwareLicense20Test {
 
-    private static final String HEADER
-            = "/*\n"
-            + " *  Copyright 2012-2013 FooBar.\n"
-            + " *\n"
-            + " *  Licensed under the Apache License, Version 2.0 (the \"License\");\n"
-            + " *  you may not use this file except in compliance with the License.\n"
-            + " *\n"
-            + " *  You may obtain a copy of the License at\n"
-            + " *       http://www.apache.org/licenses/LICENSE-2.0\n"
-            + " *\n"
-            + " *  Unless required by applicable law or agreed to in writing, software\n"
-            + " *  distributed under the License is distributed on an \"AS IS\" BASIS,\n"
-            + " *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
-            + " *  See the License for the specific language governing permissions and\n"
-            + " *  limitations under the License.\n"
-            + " */\n";
+	private static final String HEADER = "/*\n"
+			+ " *  Copyright 2012-2013 FooBar.\n"
+			+ " *\n"
+			+ " *  Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+			+ " *  you may not use this file except in compliance with the License.\n"
+			+ " *\n"
+			+ " *  You may obtain a copy of the License at\n"
+			+ " *       http://www.apache.org/licenses/LICENSE-2.0\n"
+			+ " *\n"
+			+ " *  Unless required by applicable law or agreed to in writing, software\n"
+			+ " *  distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+			+ " *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+			+ " *  See the License for the specific language governing permissions and\n"
+			+ " *  limitations under the License.\n" + " */\n";
 
-    AppliedApacheSoftwareLicense20 license;
-    
-    MockClaimReporter reporter;
+	AppliedApacheSoftwareLicense20 license;
 
-    @Before
-    public void setUp() throws Exception {
-        license = new AppliedApacheSoftwareLicense20("FooBar");
-        reporter = new MockClaimReporter();
-    }
+	MockClaimReporter reporter;
 
-    @Test
-    public void match() throws Exception {
-        BufferedReader in = new BufferedReader(new StringReader(HEADER));
-        String line = in.readLine();
-        boolean result = false;
-        final Document subject = new MockLocation("subject");
-        while (line != null) {
-            result = license.match(subject, line);
-            line = in.readLine();
-        }
-        assertTrue("Applied AL2.0 license should be matched", result);
-        license.reset();
-        result = license.match(subject, "New line");
-        assertFalse("After reset, content should build up again", result);
-    }
+	@Before
+	public void setUp() throws Exception {
+		license = new AppliedApacheSoftwareLicense20("FooBar");
+		reporter = new MockClaimReporter();
+	}
 
-    @Test
-    public void noMatch() throws Exception {
-        BufferedReader in = Resources.getBufferedResourceReader("elements/Source.java");
-        String line = in.readLine();
-        boolean result = false;
-        final Document subject = new MockLocation("subject");
-        while (line != null) {
-            result = license.match(subject, line);
-            line = in.readLine();
-        }
-        assertFalse("Applied AL2.0 license should not be matched", result);
-        license.reset();
-    }
-    @Test(timeout=2000) // may need to be adjusted if many more files are added
-    public void goodFiles() throws Exception {
-        DirectoryScanner.testFilesInDir("appliedAL20/good", license, true);
-    }
-   
-    @Test(timeout=2000) // may need to be adjusted if many more files are added
-    public void baddFiles() throws Exception {
-        DirectoryScanner.testFilesInDir("appliedAL20/bad", license, false);
-    }
-   
+	@Test
+	public void match() throws Exception {
+		BufferedReader in = new BufferedReader(new StringReader(HEADER));
+		String line = in.readLine();
+		boolean result = false;
+		final Document subject = new MockLocation("subject");
+		while (line != null) {
+			result = license.match(subject, line);
+			line = in.readLine();
+		}
+		assertTrue("Applied AL2.0 license should be matched", result);
+		license.reset();
+		result = license.match(subject, "New line");
+		assertFalse("After reset, content should build up again", result);
+	}
+
+	@Test
+	public void noMatch() throws Exception {
+		BufferedReader in = Resources
+				.getBufferedResourceReader("elements/Source.java");
+		String line = in.readLine();
+		boolean result = false;
+		final Document subject = new MockLocation("subject");
+		while (line != null) {
+			result = license.match(subject, line);
+			line = in.readLine();
+		}
+		assertFalse("Applied AL2.0 license should not be matched", result);
+		license.reset();
+	}
+
+	@Test(timeout = 2000)
+	// may need to be adjusted if many more files are added
+	public void goodFiles() throws Exception {
+		DirectoryScanner.testFilesInDir("appliedAL20/good", license, true);
+	}
+
+	@Test(timeout = 2000)
+	// may need to be adjusted if many more files are added
+	public void baddFiles() throws Exception {
+		DirectoryScanner.testFilesInDir("appliedAL20/bad", license, false);
+	}
+
 }
