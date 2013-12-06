f7a8a2e4104cfed4f728be56087d7c2a8a7497ac
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-06 18:22:44 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1548641 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/analysis/license/GPL2License.java
+++ apache-rat-core/src/main/java/org/apache/rat/analysis/license/GPL2License.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.analysis.license;
 
 import org.apache.rat.api.MetaData;
@@ -24,17 +24,17 @@ import org.apache.rat.api.MetaData;
  * Licence matches GPL2 or later.
  */
 public class GPL2License extends FullTextMatchingLicense {
-    public static final String FIRST_LICENSE_LINE = "This program is free software; you can redistribute it and/or\n" +
-            " modify it under the terms of the GNU General Public License\n" +
-            " as published by the Free Software Foundation; either version 2\n" +
-            " of the License, or (at your option) any later version.";
+	public static final String FIRST_LICENSE_LINE = "This program is free software; you can redistribute it and/or\n"
+			+ " modify it under the terms of the GNU General Public License\n"
+			+ " as published by the Free Software Foundation; either version 2\n"
+			+ " of the License, or (at your option) any later version.";
 
-    /**
+	/**
      * 
      */
-    public GPL2License() {
-        super(MetaData.RAT_LICENSE_FAMILY_CATEGORY_DATUM_GPL2,
-                MetaData.RAT_LICENSE_FAMILY_NAME_DATUM_GPL_VERSION_2,
-                "", FIRST_LICENSE_LINE);
-    }
+	public GPL2License() {
+		super(MetaData.RAT_LICENSE_FAMILY_CATEGORY_DATUM_GPL2,
+				MetaData.RAT_LICENSE_FAMILY_NAME_DATUM_GPL_VERSION_2, "",
+				FIRST_LICENSE_LINE);
+	}
 }
