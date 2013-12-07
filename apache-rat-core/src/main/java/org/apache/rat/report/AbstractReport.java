ffe7b35a20fd0ccf19241ec3bc83ee57091a0a21
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-07 11:19:13 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1548872 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/report/AbstractReport.java
+++ apache-rat-core/src/main/java/org/apache/rat/report/AbstractReport.java
@@ -15,26 +15,25 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.report;
 
 import org.apache.rat.api.Document;
 import org.apache.rat.api.RatException;
 
-
 /**
  * Abstract base class for deriving implementations of {@link RatReport}.
  */
 public abstract class AbstractReport implements RatReport {
-    public void startReport() throws RatException {
-        // Does nothing
-    }
+	public void startReport() throws RatException {
+		// Does nothing
+	}
 
-    public void report(Document document) throws RatException {
-        // Does nothing
-    }
+	public void report(Document document) throws RatException {
+		// Does nothing
+	}
 
-    public void endReport() throws RatException {
-        // Does nothing
-    }
+	public void endReport() throws RatException {
+		// Does nothing
+	}
 }
