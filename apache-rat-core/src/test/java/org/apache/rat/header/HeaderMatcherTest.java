96fa7d19a21ce96cb4234d830230c11543fcf0db
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:02:37 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549243 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/header/HeaderMatcherTest.java
+++ apache-rat-core/src/test/java/org/apache/rat/header/HeaderMatcherTest.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.header;
 
 import org.junit.Before;
@@ -30,70 +30,71 @@ import static org.junit.Assert.assertTrue;
 
 public class HeaderMatcherTest {
 
-    int capacity;
-    HeaderMatcher matcher;
-    SimpleCharFilter filter;
+	int capacity;
+	HeaderMatcher matcher;
+	SimpleCharFilter filter;
 
-    @Before
-    public void setUp() throws Exception {
-        capacity = 20;
-        filter = new SimpleCharFilter();
-        matcher = new HeaderMatcher(filter, 20);
-    }
+	@Before
+	public void setUp() throws Exception {
+		capacity = 20;
+		filter = new SimpleCharFilter();
+		matcher = new HeaderMatcher(filter, 20);
+	}
 
-    @Test
-    public void simpleMatches() throws Exception {
-        Pattern hatPattern = Pattern.compile("(.*)hat(.*)");
-        Pattern headPattern = Pattern.compile("head....");
-        StringReader reader = new StringReader("The mad hatter");
-        matcher.read(reader);
-        assertTrue(matcher.matches(hatPattern));
-        assertFalse(matcher.matches(headPattern));
-        reader = new StringReader("headache");
-        matcher.read(reader);
-        assertFalse(matcher.matches(hatPattern));
-        assertTrue(matcher.matches(headPattern));   
-    }
+	@Test
+	public void simpleMatches() throws Exception {
+		Pattern hatPattern = Pattern.compile("(.*)hat(.*)");
+		Pattern headPattern = Pattern.compile("head....");
+		StringReader reader = new StringReader("The mad hatter");
+		matcher.read(reader);
+		assertTrue(matcher.matches(hatPattern));
+		assertFalse(matcher.matches(headPattern));
+		reader = new StringReader("headache");
+		matcher.read(reader);
+		assertFalse(matcher.matches(hatPattern));
+		assertTrue(matcher.matches(headPattern));
+	}
 
-    @Test
-    public void filteredMatches() throws Exception {
-        Pattern capPattern = Pattern.compile("cap(.*)");
-        StringReader reader = new StringReader("capped");
-        matcher.read(reader);
-        assertTrue(matcher.matches(capPattern));
-        filter.filterOut = true;
-        reader = new StringReader("capped");
-        matcher.read(reader);
-        assertFalse(matcher.matches(capPattern));
-    }
+	@Test
+	public void filteredMatches() throws Exception {
+		Pattern capPattern = Pattern.compile("cap(.*)");
+		StringReader reader = new StringReader("capped");
+		matcher.read(reader);
+		assertTrue(matcher.matches(capPattern));
+		filter.filterOut = true;
+		reader = new StringReader("capped");
+		matcher.read(reader);
+		assertFalse(matcher.matches(capPattern));
+	}
 
-    @Test
-    public void noLines() throws Exception {
-        StringReader reader = new StringReader("None");
-        matcher.read(reader);
-        assertEquals("No lines read", 0, matcher.lines());
-    }
-    
-    @Test
-    public void lines() throws Exception {
-        StringReader reader = new StringReader("One\n");
-        matcher.read(reader);
-        assertEquals("One line read", 1, matcher.lines());
-        reader = new StringReader("One\nTwo");
-        matcher.read(reader);
-        assertEquals("One line read", 1, matcher.lines());
-        reader = new StringReader("One\nTwo\nThree");
-        matcher.read(reader);
-        assertEquals("Two lines read", 2, matcher.lines());
-        reader = new StringReader("One\nTwo\nThree\n");
-        matcher.read(reader);
-        assertEquals("Three lines read", 3, matcher.lines());
-    }
-    
-    @Test
-    public void tooManyLines() throws Exception {
-        StringReader reader = new StringReader("WhateverWhateverWhateverWhateverWhateverWhateverWhateverWhatever");
-        matcher.read(reader);
-        assertEquals("Too many lines read", -1, matcher.lines());
-    }
+	@Test
+	public void noLines() throws Exception {
+		StringReader reader = new StringReader("None");
+		matcher.read(reader);
+		assertEquals("No lines read", 0, matcher.lines());
+	}
+
+	@Test
+	public void lines() throws Exception {
+		StringReader reader = new StringReader("One\n");
+		matcher.read(reader);
+		assertEquals("One line read", 1, matcher.lines());
+		reader = new StringReader("One\nTwo");
+		matcher.read(reader);
+		assertEquals("One line read", 1, matcher.lines());
+		reader = new StringReader("One\nTwo\nThree");
+		matcher.read(reader);
+		assertEquals("Two lines read", 2, matcher.lines());
+		reader = new StringReader("One\nTwo\nThree\n");
+		matcher.read(reader);
+		assertEquals("Three lines read", 3, matcher.lines());
+	}
+
+	@Test
+	public void tooManyLines() throws Exception {
+		StringReader reader = new StringReader(
+				"WhateverWhateverWhateverWhateverWhateverWhateverWhateverWhatever");
+		matcher.read(reader);
+		assertEquals("Too many lines read", -1, matcher.lines());
+	}
 }
