d274a3b79e23029024c2986aa7118492052f5386
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:18:08 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549273 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/license/OASISLicenseFamily.java
+++ apache-rat-core/src/main/java/org/apache/rat/license/OASISLicenseFamily.java
@@ -15,13 +15,13 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.license;
 
 import static org.apache.rat.api.domain.RatLicenseFamily.OASIS;
 
 public final class OASISLicenseFamily extends SimpleLicenseFamily {
-    public OASISLicenseFamily() {
+	public OASISLicenseFamily() {
 		super(OASIS.getName());
-    }
+	}
 }
