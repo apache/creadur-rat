71c6ccc9f533eacd78b94067cc2b456d18b07e5f
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 19:02:47 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549112 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/document/impl/guesser/NoteGuesser.java
+++ apache-rat-core/src/main/java/org/apache/rat/document/impl/guesser/NoteGuesser.java
@@ -25,58 +25,58 @@ import org.apache.rat.api.Document;
 
 public class NoteGuesser {
 
-    private static final String[] NOTE_FILE_NAMES = { "NOTICE", "LICENSE",
-            "LICENSE.TXT", "NOTICE.TXT", "INSTALL", "INSTALL.TXT", "README",
-            "README.TXT", "NEWS", "NEWS.TXT", "AUTHOR", "AUTHOR.TXT",
-            "AUTHORS", "AUTHORS.txt", "CHANGELOG", "CHANGELOG.TXT",
-            "DISCLAIMER", "DISCLAIMER.TXT", "KEYS", "KEYS.TXT",
-            "RELEASE-NOTES", "RELEASE-NOTES.TXT", "RELEASE_NOTES",
-            "RELEASE_NOTES.TXT", "UPGRADE", "UPGRADE.TXT", "STATUS",
-            "STATUS.TXT", "THIRD_PARTY_NOTICES", "THIRD_PARTY_NOTICES.TXT",
-            "COPYRIGHT", "COPYRIGHT.TXT", "BUILDING", "BUILDING.TXT", "BUILD",
-            "BUILT.TXT", };
-    private static final String[] NOTE_FILE_EXTENSIONS = { "LICENSE",
-            "LICENSE.TXT", "NOTICE", "NOTICE.TXT", };
+	private static final String[] NOTE_FILE_NAMES = { "NOTICE", "LICENSE",
+			"LICENSE.TXT", "NOTICE.TXT", "INSTALL", "INSTALL.TXT", "README",
+			"README.TXT", "NEWS", "NEWS.TXT", "AUTHOR", "AUTHOR.TXT",
+			"AUTHORS", "AUTHORS.txt", "CHANGELOG", "CHANGELOG.TXT",
+			"DISCLAIMER", "DISCLAIMER.TXT", "KEYS", "KEYS.TXT",
+			"RELEASE-NOTES", "RELEASE-NOTES.TXT", "RELEASE_NOTES",
+			"RELEASE_NOTES.TXT", "UPGRADE", "UPGRADE.TXT", "STATUS",
+			"STATUS.TXT", "THIRD_PARTY_NOTICES", "THIRD_PARTY_NOTICES.TXT",
+			"COPYRIGHT", "COPYRIGHT.TXT", "BUILDING", "BUILDING.TXT", "BUILD",
+			"BUILT.TXT", };
+	private static final String[] NOTE_FILE_EXTENSIONS = { "LICENSE",
+			"LICENSE.TXT", "NOTICE", "NOTICE.TXT", };
 
-    private final String[] noteFileNames;
-    private final String[] noteFileExtensions;
+	private final String[] noteFileNames;
+	private final String[] noteFileExtensions;
 
-    public NoteGuesser() {
-        this(NOTE_FILE_NAMES, NOTE_FILE_EXTENSIONS);
-    }
+	public NoteGuesser() {
+		this(NOTE_FILE_NAMES, NOTE_FILE_EXTENSIONS);
+	}
 
-    public NoteGuesser(final String[] noteFileNames,
-            final String[] noteFileExtensions) {
-        super();
-        this.noteFileNames = noteFileNames;
-        this.noteFileExtensions = noteFileExtensions;
-    }
+	public NoteGuesser(final String[] noteFileNames,
+			final String[] noteFileExtensions) {
+		super();
+		this.noteFileNames = noteFileNames;
+		this.noteFileExtensions = noteFileExtensions;
+	}
 
-    /**
-     * Is a file by that name a note file?
-     */
-    private final boolean isNote(final String name) {
-        if (name == null) {
-            return false;
-        }
+	/**
+	 * Is a file by that name a note file?
+	 */
+	private final boolean isNote(final String name) {
+		if (name == null) {
+			return false;
+		}
 
-        final List<String> l = Arrays.asList(this.noteFileNames);
-        final String normalisedName = GuessUtils.normalise(name);
+		final List<String> l = Arrays.asList(this.noteFileNames);
+		final String normalisedName = GuessUtils.normalise(name);
 
-        if (l.contains(name) || l.contains(normalisedName)) {
-            return true;
-        }
+		if (l.contains(name) || l.contains(normalisedName)) {
+			return true;
+		}
 
-        for (final String element : this.noteFileExtensions) {
-            if (normalisedName.endsWith("." + element)) {
-                return true;
-            }
-        }
+		for (final String element : this.noteFileExtensions) {
+			if (normalisedName.endsWith("." + element)) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public final boolean matches(final Document document) {
-        return isNote(document.getName());
-    }
+	public final boolean matches(final Document document) {
+		return isNote(document.getName());
+	}
 }
