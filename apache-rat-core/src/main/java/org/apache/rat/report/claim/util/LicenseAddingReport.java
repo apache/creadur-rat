94978e851ad2cfb523c4fe07bbd78706776cdeb6
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:41:09 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549317 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/report/claim/util/LicenseAddingReport.java
+++ apache-rat-core/src/main/java/org/apache/rat/report/claim/util/LicenseAddingReport.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.report.claim.util;
 
 import java.io.File;
@@ -28,30 +28,33 @@ import org.apache.rat.api.MetaData.Datum;
 import org.apache.rat.api.RatException;
 import org.apache.rat.report.AbstractReport;
 
-
 public class LicenseAddingReport extends AbstractReport {
-    private final AbstractLicenceAppender appender;
+	private final AbstractLicenceAppender appender;
 
-    public LicenseAddingReport(String pCopyrightMsg, boolean pForced) {
-        appender = pCopyrightMsg == null ? new ApacheV2LicenceAppender() : new ApacheV2LicenceAppender(pCopyrightMsg);
-        appender.setForce(pForced);
-    }
+	public LicenseAddingReport(String pCopyrightMsg, boolean pForced) {
+		appender = pCopyrightMsg == null ? new ApacheV2LicenceAppender()
+				: new ApacheV2LicenceAppender(pCopyrightMsg);
+		appender.setForce(pForced);
+	}
 
-    @Override
-    public void report(org.apache.rat.api.Document document) throws RatException {
-        final MetaData metaData = document.getMetaData();
-        final Datum licenseHeader = metaData.get(MetaData.RAT_URL_HEADER_CATEGORY);
-        if (licenseHeader == null
-                ||  MetaData.RAT_LICENSE_FAMILY_CATEGORY_DATUM_UNKNOWN.getValue().equals(licenseHeader.getValue())) {
-            final File file = new File(document.getName());
-            if (file.isFile()) {
-                try {
-                    appender.append(file);
-                } catch (IOException e) {
-                    throw new RatException(e.getMessage(), e);
-                }
-            }
-        }
-        metaData.getData();
-    }
+	@Override
+	public void report(org.apache.rat.api.Document document)
+			throws RatException {
+		final MetaData metaData = document.getMetaData();
+		final Datum licenseHeader = metaData
+				.get(MetaData.RAT_URL_HEADER_CATEGORY);
+		if (licenseHeader == null
+				|| MetaData.RAT_LICENSE_FAMILY_CATEGORY_DATUM_UNKNOWN
+						.getValue().equals(licenseHeader.getValue())) {
+			final File file = new File(document.getName());
+			if (file.isFile()) {
+				try {
+					appender.append(file);
+				} catch (IOException e) {
+					throw new RatException(e.getMessage(), e);
+				}
+			}
+		}
+		metaData.getData();
+	}
 }
