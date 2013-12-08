68e312dc93952b654e566b66fa6db32c353efe77
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:08:49 +0000

    Add java doc.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549254 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/policy/DefaultPolicy.java
+++ apache-rat-core/src/main/java/org/apache/rat/policy/DefaultPolicy.java
@@ -33,11 +33,22 @@ import org.apache.rat.api.MetaData;
 import org.apache.rat.api.MetaData.Datum;
 import org.apache.rat.license.ILicenseFamily;
 
+/**
+ * The Class DefaultPolicy.
+ */
 public class DefaultPolicy {
+    /** The Constant APPROVED_LICENSES. */
     private static final String[] APPROVED_LICENSES = { APACHE.getName(),
             OASIS.getName(), W3C.getName(), W3C_DOCUMENTATION.getName(),
             TMF854.getName(), MIT.getName(), CDDL1.getName(), };
 
+    /**
+     * To names.
+     *
+     * @param approvedLicenses
+     *            the approved licenses
+     * @return the string[]
+     */
     private static final String[] toNames(
             final ILicenseFamily[] approvedLicenses) {
         String[] results = null;
@@ -51,15 +62,32 @@ public class DefaultPolicy {
         return results;
     }
 
+    /** The approved license names. */
     private final String[] approvedLicenseNames;
 
+    /**
+     * Instantiates a new default policy.
+     */
     public DefaultPolicy() {
         this(APPROVED_LICENSES);
     }
 
+    /**
+     * Instantiates a new default policy.
+     *
+     * @param approvedLicenses
+     *            the approved licenses
+     */
     public DefaultPolicy(final ILicenseFamily[] approvedLicenses) {
         this(toNames(approvedLicenses));
     }
+
+    /**
+     * Instantiates a new default policy.
+     *
+     * @param approvedLicenseNames
+     *            the approved license names
+     */
 
     public DefaultPolicy(final String[] approvedLicenseNames) {
         if (approvedLicenseNames == null) {
@@ -73,6 +101,15 @@ public class DefaultPolicy {
         Arrays.sort(this.approvedLicenseNames);
     }
 
+    /**
+     * Report license approval claim.
+     *
+     * @param subject
+     *            the subject
+     * @param isAcceptable
+     *            the is acceptable
+     */
+
     public void reportLicenseApprovalClaim(final Document subject,
             final boolean isAcceptable) {
         final Datum datum;
@@ -84,6 +121,12 @@ public class DefaultPolicy {
         subject.getMetaData().set(datum);
     }
 
+    /**
+     * Analyse.
+     *
+     * @param subject
+     *            the subject
+     */
     public void analyse(final Document subject) {
         if (subject != null) {
             final String name = subject.getMetaData().value(
