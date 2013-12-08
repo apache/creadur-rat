eae6d711ec8a208283821262e0246e9e57a218b4
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 20:55:17 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549228 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/header/FilteringSequenceFactory.java
+++ apache-rat-core/src/main/java/org/apache/rat/header/FilteringSequenceFactory.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.header;
 
 import java.io.IOException;
@@ -23,37 +23,36 @@ import java.io.Reader;
 import java.nio.CharBuffer;
 
 class FilteringSequenceFactory {
-    
-    private static final int BUFFER_CAPACITY = 5000;
-    
-    private final CharBuffer buffer;
-    private final CharFilter filter;
-    
-    public FilteringSequenceFactory(final CharFilter filter) {
-        this(BUFFER_CAPACITY, filter);
-    }
-    
-    public FilteringSequenceFactory(final int capacity, final CharFilter filter) {
-        this.buffer = CharBuffer.allocate(capacity);
-        this.filter = filter;
-    }
 
-    public CharSequence filter(Reader reader) throws IOException {
-        buffer.clear();
-        boolean eof = false;
-        while(!eof) {
-            final int next = reader.read();
-            if (next == -1 || !buffer.hasRemaining()) {
-                eof = true;
-            } else {
-                final char character = (char) next;
-                if (!filter.isFilteredOut(character))
-                {
-                    buffer.put(character); 
-                }
-            }
-        }
-        buffer.limit(buffer.position()).rewind();
-        return buffer;
-    }
+	private static final int BUFFER_CAPACITY = 5000;
+
+	private final CharBuffer buffer;
+	private final CharFilter filter;
+
+	public FilteringSequenceFactory(final CharFilter filter) {
+		this(BUFFER_CAPACITY, filter);
+	}
+
+	public FilteringSequenceFactory(final int capacity, final CharFilter filter) {
+		this.buffer = CharBuffer.allocate(capacity);
+		this.filter = filter;
+	}
+
+	public CharSequence filter(Reader reader) throws IOException {
+		buffer.clear();
+		boolean eof = false;
+		while (!eof) {
+			final int next = reader.read();
+			if (next == -1 || !buffer.hasRemaining()) {
+				eof = true;
+			} else {
+				final char character = (char) next;
+				if (!filter.isFilteredOut(character)) {
+					buffer.put(character);
+				}
+			}
+		}
+		buffer.limit(buffer.position()).rewind();
+		return buffer;
+	}
 }
