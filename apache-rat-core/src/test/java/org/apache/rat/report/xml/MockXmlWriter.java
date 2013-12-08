0530d4d2e338b252bffd1990b9bc0396808b8a70
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:26:42 +0000

    Add java doc.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549290 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/report/xml/MockXmlWriter.java
+++ apache-rat-core/src/test/java/org/apache/rat/report/xml/MockXmlWriter.java
@@ -24,45 +24,94 @@ import java.util.List;
 
 import org.apache.rat.report.xml.writer.IXmlWriter;
 
+/**
+ * The Class MockXmlWriter.
+ */
 public class MockXmlWriter implements IXmlWriter {
 
+	/** The calls. */
 	public final List<Object> calls;
 
+	/**
+	 * Instantiates a new mock xml writer.
+	 */
 	public MockXmlWriter() {
 		calls = new ArrayList<Object>();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.rat.report.xml.writer.IXmlWriter#attribute(java.lang.CharSequence
+	 * , java.lang.CharSequence)
+	 */
 	public IXmlWriter attribute(CharSequence name, CharSequence value)
 			throws IOException {
 		calls.add(new Attribute(name, value));
 		return this;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.rat.report.xml.writer.IXmlWriter#closeDocument()
+	 */
 	public IXmlWriter closeDocument() throws IOException {
 		calls.add(new CloseDocument());
 		return this;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.rat.report.xml.writer.IXmlWriter#closeElement()
+	 */
 	public IXmlWriter closeElement() throws IOException {
 		calls.add(new CloseElement());
 		return this;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.rat.report.xml.writer.IXmlWriter#content(java.lang.CharSequence
+	 * )
+	 */
 	public IXmlWriter content(CharSequence content) throws IOException {
 		calls.add(new Content(content));
 		return this;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.rat.report.xml.writer.IXmlWriter#openElement(java.lang.
+	 * CharSequence)
+	 */
 	public IXmlWriter openElement(CharSequence elementName) throws IOException {
 		calls.add(new OpenElement(elementName));
 		return this;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.rat.report.xml.writer.IXmlWriter#startDocument()
+	 */
 	public IXmlWriter startDocument() throws IOException {
 		calls.add(new StartDocument());
 		return this;
 	}
 
+	/**
+	 * Checks if is close element.
+	 * 
+	 * @param index
+	 *            the index
+	 * @return true, if is close element
+	 */
 	public boolean isCloseElement(int index) {
 		boolean result = false;
 		final Object call = calls.get(index);
@@ -70,6 +119,15 @@ public class MockXmlWriter implements IXmlWriter {
 		return result;
 	}
 
+	/**
+	 * Checks if is content.
+	 * 
+	 * @param content
+	 *            the content
+	 * @param index
+	 *            the index
+	 * @return true, if is content
+	 */
 	public boolean isContent(String content, int index) {
 		boolean result = false;
 		final Object call = calls.get(index);
@@ -80,6 +138,15 @@ public class MockXmlWriter implements IXmlWriter {
 		return result;
 	}
 
+	/**
+	 * Checks if is open element.
+	 * 
+	 * @param name
+	 *            the name
+	 * @param index
+	 *            the index
+	 * @return true, if is open element
+	 */
 	public boolean isOpenElement(String name, int index) {
 		boolean result = false;
 		final Object call = calls.get(index);
@@ -90,6 +157,17 @@ public class MockXmlWriter implements IXmlWriter {
 		return result;
 	}
 
+	/**
+	 * Checks if is attribute.
+	 * 
+	 * @param name
+	 *            the name
+	 * @param value
+	 *            the value
+	 * @param index
+	 *            the index
+	 * @return true, if is attribute
+	 */
 	public boolean isAttribute(String name, String value, int index) {
 		boolean result = false;
 		final Object call = calls.get(index);
@@ -101,35 +179,81 @@ public class MockXmlWriter implements IXmlWriter {
 		return result;
 	}
 
+	/**
+	 * The Class StartDocument.
+	 */
 	public class StartDocument {
 	}
 
+	/**
+	 * The Class CloseDocument.
+	 */
 	public class CloseDocument {
 	}
 
+	/**
+	 * The Class CloseElement.
+	 */
 	public class CloseElement {
 	}
 
+	/**
+	 * The Class OpenElement.
+	 */
 	public class OpenElement {
+
+		/** The element name. */
 		public final CharSequence elementName;
 
+		/**
+		 * Instantiates a new open element.
+		 * 
+		 * @param elementName
+		 *            the element name
+		 */
 		private OpenElement(CharSequence elementName) {
 			this.elementName = elementName;
 		}
 	}
 
+	/**
+	 * The Class Content.
+	 */
 	public class Content {
+
+		/** The content. */
 		public final CharSequence content;
 
+		/**
+		 * Instantiates a new content.
+		 * 
+		 * @param content
+		 *            the content
+		 */
 		private Content(CharSequence content) {
 			this.content = content;
 		}
 	}
 
+	/**
+	 * The Class Attribute.
+	 */
 	public class Attribute {
+
+		/** The name. */
 		public final CharSequence name;
+
+		/** The value. */
 		public final CharSequence value;
 
+		/**
+		 * Instantiates a new attribute.
+		 * 
+		 * @param name
+		 *            the name
+		 * @param value
+		 *            the value
+		 */
 		private Attribute(CharSequence name, CharSequence value) {
 			this.name = name;
 			this.value = value;
