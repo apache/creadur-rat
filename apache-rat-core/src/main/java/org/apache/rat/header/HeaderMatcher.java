5ff58bcd9c0db3ea442f5eb2a3495bb644c05043
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-08 21:00:33 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549239 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/header/HeaderMatcher.java
+++ apache-rat-core/src/main/java/org/apache/rat/header/HeaderMatcher.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat.header;
 
 import java.io.IOException;
@@ -25,80 +25,92 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * <p>Matches headers.</p>
- * <p><strong>Usage:</strong></p>
+ * <p>
+ * Matches headers.
+ * </p>
+ * <p>
+ * <strong>Usage:</strong>
+ * </p>
  * <ol>
  * <li>{@link #read(Reader)} content</li>
  * <li>{@link #matches(Pattern)} against filtered content</li>
  * </ol>
- * <p><strong>Note:</strong> use only from a single thread.</p>
- *
+ * <p>
+ * <strong>Note:</strong> use only from a single thread.
+ * </p>
+ * 
  */
 public class HeaderMatcher {
 
-    private final FilteringSequenceFactory factory;
-    private final HeaderBean[] headers;
-    private CharSequence read;
-    private int lines;
-    
-    public HeaderMatcher(final CharFilter filter, final int capacity) {
-        this(filter, capacity, null);
-    }
-    
-    public HeaderMatcher(final CharFilter filter, final int capacity, final HeaderBean[] headers) {
-        factory = new FilteringSequenceFactory(capacity, filter);
-        read = null;
-        this.headers = headers;
-    }
-    
-    public void read(Reader reader) throws IOException {
-        final LineNumberReader lineNumberReader = new LineNumberReader(reader);
-        read = factory.filter(lineNumberReader);
-        if (lineNumberReader.read() == -1) {
-            lines = lineNumberReader.getLineNumber();
-        } else {
-            lines = -1;
-        }
-        if (headers != null) {
-            final int length = headers.length;
-            for (int i=0;i<length;i++) {
-                final HeaderBean headerBean = headers[i];
-                if (headerBean != null) {
-                    final Pattern headerPattern = headerBean.getHeaderPattern();
-                    if (headerPattern != null) {
-                        final boolean matches = matches(headerPattern);
-                        headerBean.setMatch(matches);
-                    }
-                }
-            }
-        }
-    }
-    
-    /**
-     * <p>Seeks a match in the last headers read.</p>
-     * <p><strong>Note</strong> that this pattern
-     * must not contain filtered characters.
-     * </p>
-     * @param pattern <code>Pattern</code> to match
-     * @return true if the pattern matches,
-     * false otherwise or if {@link #read(Reader)} has not been
-     * called
-     */
-    public boolean matches(Pattern pattern) {
-        boolean result = false;
-        if (read != null) {
-            final Matcher matcher = pattern.matcher(read);
-            result = matcher.matches();
-        }
-        return result;
-    }
-    
-    /**
-     * Number of lines read.
-     * @return the number of lines in the file
-     * or -1 if the file has more lines than were read
-     */
-    public int lines() {
-        return lines;
-    }
+	private final FilteringSequenceFactory factory;
+	private final HeaderBean[] headers;
+	private CharSequence read;
+	private int lines;
+
+	public HeaderMatcher(final CharFilter filter, final int capacity) {
+		this(filter, capacity, null);
+	}
+
+	public HeaderMatcher(final CharFilter filter, final int capacity,
+			final HeaderBean[] headers) {
+		factory = new FilteringSequenceFactory(capacity, filter);
+		read = null;
+		this.headers = headers;
+	}
+
+	public void read(Reader reader) throws IOException {
+		final LineNumberReader lineNumberReader = new LineNumberReader(reader);
+		read = factory.filter(lineNumberReader);
+		if (lineNumberReader.read() == -1) {
+			lines = lineNumberReader.getLineNumber();
+		} else {
+			lines = -1;
+		}
+		if (headers != null) {
+			final int length = headers.length;
+			for (int i = 0; i < length; i++) {
+				final HeaderBean headerBean = headers[i];
+				if (headerBean != null) {
+					final Pattern headerPattern = headerBean.getHeaderPattern();
+					if (headerPattern != null) {
+						final boolean matches = matches(headerPattern);
+						headerBean.setMatch(matches);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * <p>
+	 * Seeks a match in the last headers read.
+	 * </p>
+	 * <p>
+	 * <strong>Note</strong> that this pattern must not contain filtered
+	 * characters.
+	 * </p>
+	 * 
+	 * @param pattern
+	 *            <code>Pattern</code> to match
+	 * @return true if the pattern matches, false otherwise or if
+	 *         {@link #read(Reader)} has not been called
+	 */
+	public boolean matches(Pattern pattern) {
+		boolean result = false;
+		if (read != null) {
+			final Matcher matcher = pattern.matcher(read);
+			result = matcher.matches();
+		}
+		return result;
+	}
+
+	/**
+	 * Number of lines read.
+	 * 
+	 * @return the number of lines in the file or -1 if the file has more lines
+	 *         than were read
+	 */
+	public int lines() {
+		return lines;
+	}
 }
