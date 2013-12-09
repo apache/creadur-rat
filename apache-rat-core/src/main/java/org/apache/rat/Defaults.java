0710bf66bca181e4b0572d6db4832e4173c84f34
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-09 06:10:03 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549471 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/Defaults.java
+++ apache-rat-core/src/main/java/org/apache/rat/Defaults.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- */ 
+ */
 package org.apache.rat;
 
 import org.apache.rat.analysis.IHeaderMatcher;
@@ -36,48 +36,39 @@ import org.apache.rat.analysis.util.HeaderMatcherMultiplexer;
 import java.io.InputStream;
 import org.apache.rat.analysis.license.CDDL1License;
 
-
-
 /**
  * Utility class that holds constants shared by the CLI tool and the Ant tasks.
  */
 public class Defaults {
-    /** no instances */
-    private Defaults() {}
+	/** no instances */
+	private Defaults() {
+	}
 
-    /**
-     * The standard list of licenses to include in the reports.
-     */
-    public static final IHeaderMatcher[] DEFAULT_MATCHERS =
-        new IHeaderMatcher[] {
-            new ApacheSoftwareLicense20(),
-            new GPL1License(),
-            new GPL2License(),
-            new GPL3License(),
-            new MITLicense(),
-            new W3CLicense(),
-            new W3CDocLicense(),
-            new OASISLicense(),
-            new JavaDocLicenseNotRequired(), 
-            new GeneratedLicenseNotRequired(),
-            new DojoLicenseHeader(),
-            new TMF854LicenseHeader(),
-            new CDDL1License(),
-    };
-    
-    public static final String PLAIN_STYLESHEET = "org/apache/rat/plain-rat.xsl";
-    
-    public static InputStream getPlainStyleSheet() {
-        InputStream result = Defaults.class.getClassLoader().getResourceAsStream(Defaults.PLAIN_STYLESHEET);
-        return result;
-    }
-    
-    public static InputStream getDefaultStyleSheet() {
-        InputStream result = getPlainStyleSheet();
-        return result;
-    }
-    
-    public static IHeaderMatcher createDefaultMatcher() {
-        return new HeaderMatcherMultiplexer(Defaults.DEFAULT_MATCHERS);
-    }
+	/**
+	 * The standard list of licenses to include in the reports.
+	 */
+	public static final IHeaderMatcher[] DEFAULT_MATCHERS = new IHeaderMatcher[] {
+			new ApacheSoftwareLicense20(), new GPL1License(),
+			new GPL2License(), new GPL3License(), new MITLicense(),
+			new W3CLicense(), new W3CDocLicense(), new OASISLicense(),
+			new JavaDocLicenseNotRequired(), new GeneratedLicenseNotRequired(),
+			new DojoLicenseHeader(), new TMF854LicenseHeader(),
+			new CDDL1License(), };
+
+	public static final String PLAIN_STYLESHEET = "org/apache/rat/plain-rat.xsl";
+
+	public static InputStream getPlainStyleSheet() {
+		InputStream result = Defaults.class.getClassLoader()
+				.getResourceAsStream(Defaults.PLAIN_STYLESHEET);
+		return result;
+	}
+
+	public static InputStream getDefaultStyleSheet() {
+		InputStream result = getPlainStyleSheet();
+		return result;
+	}
+
+	public static IHeaderMatcher createDefaultMatcher() {
+		return new HeaderMatcherMultiplexer(Defaults.DEFAULT_MATCHERS);
+	}
 }
