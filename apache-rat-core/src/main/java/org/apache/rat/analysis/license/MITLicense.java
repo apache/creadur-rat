6d1b0a9d46153894489ccc8a1e1ff047aad53fdf
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-09 05:54:02 +0000

    Add Java Doc.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549435 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/main/java/org/apache/rat/analysis/license/MITLicense.java
+++ apache-rat-core/src/main/java/org/apache/rat/analysis/license/MITLicense.java
@@ -24,13 +24,19 @@ import static org.apache.rat.api.domain.RatLicenseFamily.MIT;
  * Base MIT license (all 3 parts).
  */
 public class MITLicense extends FullTextMatchingLicense {
+
+	/** The Constant FIRST_LICENSE_LINE. */
 	public static final String FIRST_LICENSE_LINE = "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:";
+
+	/** The Constant MIDDLE_LICENSE_LINE. */
 	public static final String MIDDLE_LICENSE_LINE = "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.";
+
+	/** The Constant AS_IS_LICENSE_LINE. */
 	public static final String AS_IS_LICENSE_LINE = "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
 
 	/**
-     * 
-     */
+	 * Instantiates a new mIT license.
+	 */
 	public MITLicense() {
 		super(MIT.licenseFamily(),
 				FIRST_LICENSE_LINE + MIDDLE_LICENSE_LINE + AS_IS_LICENSE_LINE);
