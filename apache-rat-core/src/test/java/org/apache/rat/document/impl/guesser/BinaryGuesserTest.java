b626e30b3aec21a916af5b23998b031313bd7f76
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-09 05:45:39 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549417 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/document/impl/guesser/BinaryGuesserTest.java
+++ apache-rat-core/src/test/java/org/apache/rat/document/impl/guesser/BinaryGuesserTest.java
@@ -32,85 +32,82 @@ import org.junit.Test;
 
 public class BinaryGuesserTest {
 
-    @Test
-    public void testMatches() {
-        assertThatDocumentIsBinary("image.png");
-        assertThatDocumentIsBinary("image.pdf");
-        assertThatDocumentIsBinary("image.gif");
-        assertThatDocumentIsBinary("image.giff");
-        assertThatDocumentIsBinary("image.tif");
-        assertThatDocumentIsBinary("image.tiff");
-        assertThatDocumentIsBinary("image.jpg");
-        assertThatDocumentIsBinary("image.jpeg");
-        assertThatDocumentIsBinary("image.exe");
-        assertThatDocumentIsBinary("Whatever.class");
-        assertThatDocumentIsBinary("data.dat");
-        assertThatDocumentIsBinary("libicudata.so.34.");
-    }
+	@Test
+	public void testMatches() {
+		assertThatDocumentIsBinary("image.png");
+		assertThatDocumentIsBinary("image.pdf");
+		assertThatDocumentIsBinary("image.gif");
+		assertThatDocumentIsBinary("image.giff");
+		assertThatDocumentIsBinary("image.tif");
+		assertThatDocumentIsBinary("image.tiff");
+		assertThatDocumentIsBinary("image.jpg");
+		assertThatDocumentIsBinary("image.jpeg");
+		assertThatDocumentIsBinary("image.exe");
+		assertThatDocumentIsBinary("Whatever.class");
+		assertThatDocumentIsBinary("data.dat");
+		assertThatDocumentIsBinary("libicudata.so.34.");
+	}
 
-    private void assertThatDocumentIsBinary(final String name) {
-        assertTrue(new BinaryGuesser().matches(new MockDocument(name)));
-    }
+	private void assertThatDocumentIsBinary(final String name) {
+		assertTrue(new BinaryGuesser().matches(new MockDocument(name)));
+	}
 
-    /**
-     * Used to swallow a MalformedInputException and return false because the
-     * encoding of the stream was different from the platform's default
-     * encoding.
-     * 
-     * @see "RAT-81"
-     */
-    @Test
-    public void binaryWithMalformedInputRAT81() throws Throwable {
-        Document doc =
-                new FileDocument(new File(
-                        "src/test/resources/binaries/UTF16_with_signature.xml"));
-        Reader r = null;
-        try {
-            final char[] dummy = new char[100];
-            r = doc.reader();
-            r.read(dummy);
-            // if we get here, the UTF-16 encoded file didn't throw
-            // any exception, try the UTF-8 encoded one
-            r.close();
-            doc =
-                    new FileDocument(
-                            new File(
-                                    "src/test/resources/binaries/UTF8_with_signature.xml"));
-            r = doc.reader();
-            r.read(dummy);
-            // still here? can't test on this platform
-            System.err.println("Skipping testBinaryWithMalformedInput");
-        } catch (final IOException e) {
-            if (r != null) {
-                r.close();
-            }
-            r = null;
-            assertTrue(new BinaryGuesser().matches(doc));
-        } finally {
-            if (r != null) {
-                r.close();
-            }
-        }
-    }
+	/**
+	 * Used to swallow a MalformedInputException and return false because the
+	 * encoding of the stream was different from the platform's default
+	 * encoding.
+	 * 
+	 * @see "RAT-81"
+	 */
+	@Test
+	public void binaryWithMalformedInputRAT81() throws Throwable {
+		Document doc = new FileDocument(new File(
+				"src/test/resources/binaries/UTF16_with_signature.xml"));
+		Reader r = null;
+		try {
+			final char[] dummy = new char[100];
+			r = doc.reader();
+			r.read(dummy);
+			// if we get here, the UTF-16 encoded file didn't throw
+			// any exception, try the UTF-8 encoded one
+			r.close();
+			doc = new FileDocument(new File(
+					"src/test/resources/binaries/UTF8_with_signature.xml"));
+			r = doc.reader();
+			r.read(dummy);
+			// still here? can't test on this platform
+			System.err.println("Skipping testBinaryWithMalformedInput");
+		} catch (final IOException e) {
+			if (r != null) {
+				r.close();
+			}
+			r = null;
+			assertTrue(new BinaryGuesser().matches(doc));
+		} finally {
+			if (r != null) {
+				r.close();
+			}
+		}
+	}
 
-    @Test
-    public void realBinaryContent() {
-        // This test is not accurate on all platforms
-        if (System.getProperty("file.encoding").startsWith("ANSI")) {
-            assertTrue(new BinaryGuesser().matches(new FileDocument(new File(
-            "src/test/resources/binaries/Image-png.not"))));
-        }
-    }
+	@Test
+	public void realBinaryContent() {
+		// This test is not accurate on all platforms
+		if (System.getProperty("file.encoding").startsWith("ANSI")) {
+			assertTrue(new BinaryGuesser().matches(new FileDocument(new File(
+					"src/test/resources/binaries/Image-png.not"))));
+		}
+	}
 
-    @Test
-    public void textualContent() {
-        assertFalse(new BinaryGuesser().matches(new FileDocument(new File(
-        "src/test/resources/elements/Text.txt"))));
-    }
+	@Test
+	public void textualContent() {
+		assertFalse(new BinaryGuesser().matches(new FileDocument(new File(
+				"src/test/resources/elements/Text.txt"))));
+	}
 
-    @Test
-    public void emptyFile() {
-        assertFalse(new BinaryGuesser().matches(new FileDocument(new File(
-        "src/test/resources/elements/sub/Empty.txt"))));
-    }
+	@Test
+	public void emptyFile() {
+		assertFalse(new BinaryGuesser().matches(new FileDocument(new File(
+				"src/test/resources/elements/sub/Empty.txt"))));
+	}
 }
