72e7765fc4d03f17368320cef409ed99163290b3
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-09 05:47:32 +0000

    Format Code.
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549423 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/document/impl/guesser/NoteGuesserTest.java
+++ apache-rat-core/src/test/java/org/apache/rat/document/impl/guesser/NoteGuesserTest.java
@@ -25,29 +25,29 @@ import org.junit.Test;
 
 public class NoteGuesserTest {
 
-    @Test
-    public void testMatches() {
-        assertThatGuesserMatches("LICENSE");
-        assertThatGuesserMatches("LICENSE.txt");
-        assertThatGuesserMatches("NOTICE");
-        assertThatGuesserMatches("NOTICE.txt");
-        assertThatGuesserMatches("README");
-        assertThatGuesserMatches("README.txt");
-        assertThatGuesserMatches("src/test/LICENSE");
-        assertThatGuesserMatches("src/test/LICENSE.txt");
-        assertThatGuesserMatches("src/test/NOTICE");
-        assertThatGuesserMatches("src/test/NOTICE.txt");
-        assertThatGuesserMatches("src/test/README");
-        assertThatGuesserMatches("src/test/README.txt");
-        assertThatGuesserMatches("src\\test\\LICENSE");
-        assertThatGuesserMatches("src\\test\\LICENSE.txt");
-        assertThatGuesserMatches("src\\test\\NOTICE");
-        assertThatGuesserMatches("src\\test\\NOTICE.txt");
-        assertThatGuesserMatches("src\\test\\README");
-        assertThatGuesserMatches("src\\test\\README.txt");
-    }
+	@Test
+	public void testMatches() {
+		assertThatGuesserMatches("LICENSE");
+		assertThatGuesserMatches("LICENSE.txt");
+		assertThatGuesserMatches("NOTICE");
+		assertThatGuesserMatches("NOTICE.txt");
+		assertThatGuesserMatches("README");
+		assertThatGuesserMatches("README.txt");
+		assertThatGuesserMatches("src/test/LICENSE");
+		assertThatGuesserMatches("src/test/LICENSE.txt");
+		assertThatGuesserMatches("src/test/NOTICE");
+		assertThatGuesserMatches("src/test/NOTICE.txt");
+		assertThatGuesserMatches("src/test/README");
+		assertThatGuesserMatches("src/test/README.txt");
+		assertThatGuesserMatches("src\\test\\LICENSE");
+		assertThatGuesserMatches("src\\test\\LICENSE.txt");
+		assertThatGuesserMatches("src\\test\\NOTICE");
+		assertThatGuesserMatches("src\\test\\NOTICE.txt");
+		assertThatGuesserMatches("src\\test\\README");
+		assertThatGuesserMatches("src\\test\\README.txt");
+	}
 
-    private void assertThatGuesserMatches(final String name) {
-        assertTrue(new NoteGuesser().matches(new MockDocument(name)));
-    }
+	private void assertThatGuesserMatches(final String name) {
+		assertTrue(new NoteGuesser().matches(new MockDocument(name)));
+	}
 }
