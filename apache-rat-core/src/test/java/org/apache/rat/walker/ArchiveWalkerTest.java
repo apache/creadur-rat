ecef229443a8c6afd708a396d689aba22969b7f2
Robert Burrell Donkin <rdonkin@apache.org>
2013-12-09 06:09:49 +0000

    Add Test
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/branches/gsoc@1549470 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-core/src/test/java/org/apache/rat/walker/ArchiveWalkerTest.java
+++ apache-rat-core/src/test/java/org/apache/rat/walker/ArchiveWalkerTest.java
@@ -22,7 +22,13 @@ package org.apache.rat.walker;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.rat.report.RatReport;
+import org.apache.rat.report.claim.util.Pipeline;
+import org.apache.rat.test.utils.Resources;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -44,6 +50,53 @@ public class ArchiveWalkerTest {
 		FilenameFilter filter = null;
 		ArchiveWalker archiveWalker = new ArchiveWalker(file, filter);
 		Assert.assertNotNull(archiveWalker);
+	}
+
+	/**
+	 * Test run.
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void testRun() throws IOException {
+		File file = Resources.getResourceFile("elements/Source.java");
+		FilenameFilter filter = null;
+		ArchiveWalker archiveWalker = new ArchiveWalker(file, filter);
+		RatReport report = new Pipeline(null, null, null);
+		archiveWalker.run(report);
+	}
+
+	/**
+	 * Test run gz file.
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void testRunGZFile() throws IOException {
+		File file = Resources.getResourceFile("artifacts/dummy.tar.gz");
+		FilenameFilter filter = null;
+		ArchiveWalker archiveWalker = new ArchiveWalker(file, filter);
+		List<? extends RatReport> listReport = new ArrayList<RatReport>();
+		RatReport report = new Pipeline(null, null, listReport);
+		archiveWalker.run(report);
+	}
+
+	/**
+	 * Test run zip file.
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void testRunZipFile() throws IOException {
+		File file = Resources.getResourceFile("artifacts/Source.java.bz2");
+		FilenameFilter filter = null;
+		ArchiveWalker archiveWalker = new ArchiveWalker(file, filter);
+		List<? extends RatReport> listReport = new ArrayList<RatReport>();
+		RatReport report = new Pipeline(null, null, listReport);
+		archiveWalker.run(report);
 	}
 
 }
