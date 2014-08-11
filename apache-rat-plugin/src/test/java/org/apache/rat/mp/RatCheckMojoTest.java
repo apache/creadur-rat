301808b9ffa29158df21daf17795ccbf5410323b
Sebastian Bazley <sebb@apache.org>
2014-08-11 11:36:54 +0000

    assertions are not related to RAT-168
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/trunk@1617257 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-plugin/src/test/java/org/apache/rat/mp/RatCheckMojoTest.java
+++ apache-rat-plugin/src/test/java/org/apache/rat/mp/RatCheckMojoTest.java
@@ -177,9 +177,9 @@ public class RatCheckMojoTest extends AbstractMojoTestCase
             }
         };
         setVariableValueToObject( mojo, "project", project );
-        assertNotNull("RAT-168: Problem in test setup - you are missing a project in your mojo.", project);
-        assertNotNull("RAT-168: The mojo is missing its MavenProject, which will result in an NPE during rat runs.", mojo.getProject());
-        assertNotNull("RAT-168: No artifactRepos found, which will result in an NPE during rat runs.", project.getRemoteArtifactRepositories());
+        assertNotNull("Problem in test setup - you are missing a project in your mojo.", project);
+        assertNotNull("The mojo is missing its MavenProject, which will result in an NPE during rat runs.", mojo.getProject());
+        assertNotNull("No artifactRepos found, which will result in an NPE during rat runs.", project.getRemoteArtifactRepositories());
         if (mojo instanceof RatReportMojo)
         {
             setVariableValueToObject( mojo, "localRepository", newArtifactRepository() );
