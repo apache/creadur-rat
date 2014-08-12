2159246f723005f557138178f3ca8644c35952a3
Sebastian Bazley <sebb@apache.org>
2014-08-12 01:38:23 +0000

    Anglicise
    git-svn-id: https://svn.apache.org/repos/asf/creadur/rat/trunk@1617393 13f79535-47bb-0310-9956-ffa450edef68

--- apache-rat-plugin/src/test/java/org/apache/rat/mp/AbstractRatMojoTest.java
+++ apache-rat-plugin/src/test/java/org/apache/rat/mp/AbstractRatMojoTest.java
@@ -24,10 +24,10 @@ import org.junit.Test;
 public class AbstractRatMojoTest {
 
     @Test
-    public void testAmountOfExclusions() {
-        assertEquals("Did you change the amount of eclipse excludes?", 3, AbstractRatMojo.ECLIPSE_DEFAULT_EXCLUDES.size());
-        assertEquals("Did you change the amount of idea excludes?", 4, AbstractRatMojo.IDEA_DEFAULT_EXCLUDES.size());
-        assertEquals("Did you change the amount of mvn excludes?", 4, AbstractRatMojo.MAVEN_DEFAULT_EXCLUDES.size());
+    public void testNumberOfExclusions() {
+        assertEquals("Did you change the number of eclipse excludes?", 3, AbstractRatMojo.ECLIPSE_DEFAULT_EXCLUDES.size());
+        assertEquals("Did you change the number of idea excludes?", 4, AbstractRatMojo.IDEA_DEFAULT_EXCLUDES.size());
+        assertEquals("Did you change the number of mvn excludes?", 4, AbstractRatMojo.MAVEN_DEFAULT_EXCLUDES.size());
     }
 
 }
