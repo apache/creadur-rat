c2485e603fb32f566890192e30cb4a011cf520bf
P. Ottlinger <pottlinger@apache.org>
2022-08-15 00:15:22 +0200

    RAT-310: Use hamcrest assertion instead of junit one's

--- apache-rat-api/src/test/java/org/apache/rat/api/domain/LicenseFamilyBuilderTest.java
+++ apache-rat-api/src/test/java/org/apache/rat/api/domain/LicenseFamilyBuilderTest.java
@@ -19,8 +19,8 @@
 package org.apache.rat.api.domain;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
