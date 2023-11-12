ffc9027d50b9aae90f7d4c85622c2c15e8d74e9b
P. Ottlinger <pottlinger@apache.org>
2023-11-12 20:51:33 +0100

    RAT-326: Fix javadoc warnings

--- apache-rat-api/src/main/java/org/apache/rat/api/domain/RatLicenseFamily.java
+++ apache-rat-api/src/main/java/org/apache/rat/api/domain/RatLicenseFamily.java
@@ -25,47 +25,81 @@ import static org.apache.rat.api.domain.LicenseFamilyBuilder.aLicenseFamily;
  */
 public enum RatLicenseFamily {
 
+	/**
+	 * Apache License Version 2.0
+	 */
 	APACHE(
 			"Apache License Version 2.0",
 			"AL   ",
 			"Note that APACHE requires a NOTICE. All modifications require notes. See http://www.apache.org/licenses/LICENSE-2.0."),
+	/**
+	 * GNU General Public License, version 1
+	 */
 	GPL1(
 			"GNU General Public License, version 1",
 			"GPL1 ",
 			"Note that GPL1 requires a NOTICE. All modifications require notes. See http://www.gnu.org/licenses/gpl-1.0.html."),
+	/**
+	 * GNU General Public License, version 2
+	 */
 	GPL2(
 			"GNU General Public License, version 2",
 			"GPL2 ",
 			"Note that GPL2 requires a NOTICE. All modifications require notes. See http://www.gnu.org/licenses/gpl-2.0.html."),
+	/**
+	 * GNU General Public License, version 3
+	 */
 	GPL3(
 			"GNU General Public License, version 3",
 			"GPL3 ",
 			"Note that GPL3 requires a NOTICE. All modifications require notes. See http://www.gnu.org/licenses/gpl-3.0.html."),
+	/**
+	 * The MIT License
+	 */
 	MIT(
 			"The MIT License",
 			"MIT  ",
 			"Note that MIT requires a NOTICE. All modifications require notes. See http://opensource.org/licenses/MIT."),
+
+	/**
+	 * COMMON DEVELOPMENT AND DISTRIBUTION LICENSE Version 1.0
+	 */
 	CDDL1(
 			"COMMON DEVELOPMENT AND DISTRIBUTION LICENSE Version 1.0",
 			"CDDL1",
 			"Note that CDDL1 requires a NOTICE. All modifications require notes. See https://oss.oracle.com/licenses/CDDL."),
+	/**
+	 * OASIS Open License
+	 */
 	OASIS(
 			"OASIS Open License",
 			"OASIS",
 			"Note that OASIS requires a NOTICE. All modifications require notes. See https://www.oasis-open.org/policies-guidelines/ipr."),
+	/**
+	 * Modified BSD License
+	 */
 	TMF854(
 			"Modified BSD License",
 			"TMF  ",
 			"Note that TMF854 requires a NOTICE. All modifications require notes. See http://opensource.org/licenses/BSD-3-Clause."),
-	 DOJO(
+	/**
+	 * Modified BSD License
+ 	 */
+	DOJO(
 			"Modified BSD License",
 			"DOJO ",
 			"Note that DOJO requires a NOTICE. All modifications require notes. See http://dojotoolkit.org/community/licensing.shtml."),
-    W3C(
+	/**
+	 * W3C Software Copyright
+	 */
+	W3C(
             "W3C Software Copyright",
             "W3C  ",
             "Note that W3C requires a NOTICE. All modifications require notes. See http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231."),
 
+	/**
+	 * W3C Document Copyright
+	 */
     W3C_DOCUMENTATION(
             "W3C Document Copyright",
             "W3CD ",
